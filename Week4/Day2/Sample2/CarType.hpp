2201fc5d6a75290606ac211b4ddc11a452c0de9d
Mohd Hamza Siddiqui <100111457+mhamza08@users.noreply.github.com>
2024-08-22 16:04:38 +0530

    Add files via upload

---
+++ Week4/Day2/Sample2/CarType.hpp
@@ -0,0 +1,17 @@
+#ifndef CARTYPE_HPP
+
+#define CARTYPE_HPP
+
+/*
+    data : real-value
+    categorical: moths in a year
+
+*/
+enum class CarType
+{
+    SEDAN,
+    SUV,
+    HATCHBATCH
+};
+
+#endif
